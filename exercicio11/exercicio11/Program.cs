d01d5c72995621349095f50a5f87fc73070e6ed8
adriano.santos <adriano.santos@squadra.com.br>
2019-05-29 15:06:27 -0300

    exercicio11

---
+++ exercicio11/exercicio11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 0;
+            double resultado = 0;
+            Console.Write("digite um número: ");
+            n = double.Parse(Console.ReadLine());
+            resultado = n / 3;
+            Console.WriteLine("A terça parte deste número é: {0:#.00}", resultado);
+
+        }
+    }
+}
